6ce75b4e3897cee5470185b3fefba3c8754a7da9
Leyberson <leyberson1@gmail.com>
2021-03-28 19:49:18 -0300

    verde

--- tp_4/cartao.py
+++ tp_4/cartao.py
@@ -3,3 +3,14 @@ class Cartao:
         self.limite_total = 1000
         self.limite_utilizado = 0
         self.valor_fatura = 0
+
+    def comprar(self, valor, total_de_parcelas):
+        self.limite_utilizado+=valor
+        acrescimo = valor/total_de_parcelas
+        self.valor_fatura += acrescimo
+
+    def get_valor_fatura(self):
+        if self.limite_utilizado == 0:
+            return 0
+        else:
+            return self.valor_fatura
